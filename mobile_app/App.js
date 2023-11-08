ed0de3010fa240b2e338eea1bbc1e24a3065e052
WheatleyTheCore <alex.colwell@icloud.com>
2023-11-08 10:50:05 -0500

    recording now has appropriate delays and end-trimming

--- mobile_app/App.js
+++ mobile_app/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Accelerometer, Gyroscope } from 'expo-sensors';
+import { Audio } from 'expo-av';
 import { saveDataAsCSV } from './utils/saveDataAsCSV';
 
 export default function App() {
@@ -18,6 +19,9 @@ export default function App() {
 
   const [accelSubscription, setAccelSubscription] = useState(null);
   const [gyroSubscription, setGyroSubscription] = useState(null);
+
+  const [startSound, setStartSound] = useState()
+  const [endSound, setEndSound] = useState()
 
 
   // id for setInterval that we use for recording sensor data.
@@ -37,33 +41,75 @@ export default function App() {
     setGyroSubscription(null)
   };
 
-  const startRecordingSensorData = () => {
-    intervalIdRef.current = []
+  const delay = (delayInms) => {
+    return new Promise(resolve => setTimeout(resolve, delayInms));
+  };
+
+  const startRecordingSensorData = async () => {
+
+    jsonData.current = []
+
+    // load sounds
+    console.log('Loading Sound');
+    const startSound = new Audio.Sound();
+    await startSound.loadAsync(require('./assets/mariostart.mp3'), {shouldPlay: true})
+    setStartSound(startSound)
+
+    console.log('Playing Sound');
+    await startSound.setPositionAsync(0);
+    await startSound.playAsync()
+
+    await delay(5000) // wait 5 seconds for the sound to finish playing. Definitely a better way to do this but I don't have time to find it atm
+
+    console.log('recording!')
     intervalIdRef.current = setInterval(() => {
-      let currentJsonData = jsonData.current
-      currentJsonData.push({
-        "AccelX": accelData.x.toString(),
-        "AccelY": accelData.y.toString(),
-        "AccelZ": accelData.z.toString(),
-        "GyroX": gyroData.x.toString(),
-        "GyroY": gyroData.y.toString(),
-        "GyroZ": gyroData.z.toString()
+      let updatedJsonData = jsonData.current
+      setAccelData(accelData => { // using the set functions to get state within the setinterval is hacky and very bad
+        setGyroData(gyroData => { // this is horrible for performance and my sanity
+          updatedJsonData.push({
+            "AccelX": accelData.x.toString(),
+            "AccelY": accelData.y.toString(),
+            "AccelZ": accelData.z.toString(),
+            "GyroX": gyroData.x.toString(),
+            "GyroY": gyroData.y.toString(),
+            "GyroZ": gyroData.z.toString()
+          })
+          return gyroData
+        })
+        return accelData
       })
+      jsonData.current = updatedJsonData
     }, 50)
   }
 
   const stopRecordingSensorData = () => {
-    clearInterval(intervalIdRef.current)
-    saveDataAsCSV(jsonData.current)
+    if (intervalIdRef.current) {
+      clearInterval(intervalIdRef.current)
+      intervalIdRef.current = []
+      let processedJsonData = [...jsonData.current]
+      //console.log(Array.isArray(processedJsonData))
+      jsonData.current = []
+      for (let i = 0; i < 100; i++) { //remove stuff from last 5 sconds (i.e. last 100 elements since we record every 50 ms). also kinda hacky
+        processedJsonData.pop()
+      }
+      saveDataAsCSV(processedJsonData)
+    }
   }
 
   useEffect(() => {
     _subscribe()
     Accelerometer.setUpdateInterval(30);
     Gyroscope.setUpdateInterval(30);
-    return () => _unsubscribe();
+    Audio.setAudioModeAsync({
+      playsInSilentModeIOS: true,
+  })
+    return () => {
+      _unsubscribe();
+      if (startSound) startSound.unloadAsync();
+      if (endSound) endSound.unloadAsync();
+    }
   }, []);
-  
+
 
   return (
     <View style={styles.container}>
