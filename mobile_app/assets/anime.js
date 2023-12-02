905f720010bba4395d326cedd05c7d08afb53b94
LukaTree7 <143844079+LukaTree7@users.noreply.github.com>
2023-12-02 14:07:52 -0500

    Add files via upload

---
+++ mobile_app/assets/anime.js
@@ -0,0 +1,10 @@
+<script src='js/particles.js'></script>
+<script src='js/anime.min.js'></script>
+
+<button >Button</button>
+
+// Initialize a new instance of Particles to disintegrate/integrate the button
+var particles = new Particles('.button');
+// Disintegrate the button into particles
+particles.disintegrate();
+// particles.integrate(); // would do the opposite
